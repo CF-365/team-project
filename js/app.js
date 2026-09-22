ad0e365cd7bcffc65b28082fc25f0efba58a828f
CF-365 <cfellers@bellsouth.net>
2026-09-22 12:34:42 -0400

    Add mark priority feature

--- js/app.js
+++ js/app.js
@@ -32,3 +32,12 @@ clearCompletedBtn.addEventListener("click", function () {
     task.remove();
   });
 });
+// Mark priority feature
+const priorityTasks = document.querySelectorAll("#task-list li");
+for (var i = 0; i < priorityTasks.length; i++) {
+  var currentTask = priorityTasks[i];
+  currentTask.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    currentTask.classList.toggle("priority");
+  });
+}
