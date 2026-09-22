851632aef9723cd2accc92182978d6c55fd0af0d
CF-365 <cfellers@bellsouth.net>
2026-09-22 08:56:27 -0400

    Add clear completed tas feature

--- js/app.js
+++ js/app.js
@@ -24,4 +24,11 @@ taskList.addEventListener("click", function (event) {
     event.target.classList.toggle("completed");
   }
 });
-  
+  // Clear completed tasks feature
+const clearCompletedBtn = document.getElementById("clear-completed-btn");
+clearCompletedBtn.addEventListener("click", function () {
+  const completedTasks = document.getElementsByClassName("completed");
+  for (let i = 0; i < completedTasks.length; i++) {
+    completedTasks[i].remove();
+  }
+});
